be544ef8989950fa149c1ae575f367069d41babe
github username <github email address>
2020-03-10 13:00:46 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,37 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }
+    )
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={(event) => { event.preventDefault();
+        (this.state.username.length > 0 && this.state.password.length > 0) ? this.props.handleLogin(this.state) : null }}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.username} onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,27 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      remainingCharacters: props.maxChars
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+        message: event.target.value,
+        remainingCharacters: (this.props.maxChars - event.target.value.length)
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message}  onChange={this.handleChange}/>
+        Remaining CHaractors: {this.state.remainingCharacters}
       </div>
     );
   }
